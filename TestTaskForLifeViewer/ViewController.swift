a19d4501d08f7ef1963117bc0960ccbeb1b892c6
geo201213ua@gmail.com <geo201213ua@gmail.com>
2020-04-30 19:34:52 +0300

    Initial commit without pod's

---
+++ TestTaskForLifeViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTaskForLifeViewer
+//
+//  Created by Georg Lyurko on 30.04.2020.
+//  Copyright © 2020 Georg Lyurko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
